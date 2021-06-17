14e03d365dea59226278ea69836cf896a61825da
shentiaspirit <ahmadreza.shamimi@Gmail.com>
2021-06-16 21:24:36 -0400

    hello

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,3 +5,4 @@ const description = `((${currentResult} + 2) / 2) + 10`;
 currentResult = (currentResult + 2) / 2 + 10;
 
 outputResult(currentResult, description);
+outputResult(description, currentResult);
