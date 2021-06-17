1805adbfd38eeae05082de83e52148638f074ba2
shentiaspirit <shentia@Ahmadrezas-MacBook-Pro.local>
2021-06-16 21:08:48 -0400

    first commit

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,8 @@
 let currentResult = 0;
+
+const description = `((${currentResult} + 2) / 2) + 10`;
+
+currentResult = (currentResult + 2) / 2 + 10;
+
+outputResult(currentResult, description);
+outputResult(description, currentResult);
