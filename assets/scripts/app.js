1cab8bc896892e1d3e9f2c04352fd52f4a5b400c
shentiaspirit <ahmadreza.shamimi@Gmail.com>
2021-06-18 16:16:17 -0400

    Function Add EventListener

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,14 +1,9 @@
-let currentResult = 0;
+const defaultResult = 0;
+let currentResult = defaultResult;
 
-function add(num1, num2) {
-  const result = num1 + num2;
-  return result;
+function add() {
+  currentResult = currentResult + parseInt(userInput.value);
+  outputResult(currentResult, "");
 }
 
-let sum = add(4, 4);
-
-const description = `((${currentResult} + 2) / 2) + 10`;
-
-currentResult = (currentResult + 2) / 2 + 10;
-
-outputResult(sum, description);
+addBtn.addEventListener("click", add);
