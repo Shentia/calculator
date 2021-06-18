e7f2c99aa746ea8c6bae21ec0281ffd0b42fb354
shentiaspirit <ahmadreza.shamimi@Gmail.com>
2021-06-17 21:38:55 -0400

    changes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,14 @@
 let currentResult = 0;
 
+function add(num1, num2) {
+  const result = num1 + num2;
+  return result;
+}
+
+let sum = add(4, 4);
+
 const description = `((${currentResult} + 2) / 2) + 10`;
 
 currentResult = (currentResult + 2) / 2 + 10;
 
-outputResult(currentResult, description);
+outputResult(sum, description);
