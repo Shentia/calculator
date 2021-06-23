ac1ca4c6332038c6d402786512106366971e83f6
shentiaspirit <ahmadreza.shamimi@Gmail.com>
2021-06-23 16:49:36 -0400

    Add Function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,14 +1,21 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let valueInput = userInput;
 
+function getUserInputNumber() {
+  return parseInt(userInput.value);
+}
 function add() {
-  currentResult = currentResult + parseInt(userInput.value);
-  outputResult(currentResult, "");
+  const valueInput = getUserInputNumber();
+  const calcDescription = `${currentResult} + ${valueInput}`;
+  currentResult = currentResult + valueInput;
+  outputResult(currentResult, calcDescription);
 }
-function subtract() {
-  currentResult = currentResult - parseInt(userInput.value);
-  outputResult(currentResult, "");
-}
+// function subtract() {
+//   const calcDescription = `${currentResult} - ${userInput.value}`;
+//   currentResult = currentResult - parseInt(userInput.value);
+//   outputResult(currentResult, calcDescription);
+// }
 
 addBtn.addEventListener("click", add);
 subtractBtn.addEventListener("click", subtract);
